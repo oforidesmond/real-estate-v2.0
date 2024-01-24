6f8ba4529c0ba034b585224be811373c9b08565b
Desmond Ofori <oforidesmond@rocketmail.com>
2024-01-24 15:23:31 +0000

    modify sign in auth

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -47,10 +47,10 @@ export default function SignIn() {
       <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
       <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
         <input
-          type='email'
-          placeholder='email'
+          type='text'
+          placeholder='username'
           className='border p-3 rounded-lg'
-          id='email'
+          id='username'
           onChange={handleChange}
         />
         <input
